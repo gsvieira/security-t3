105f414cdcd04a74cb75a900b04748dcc2db5a4b
gabriel_s_vieira <gsvieira98@gmail.com>
2021-10-20 17:51:08 -0300

    decoder/encoder test

--- base64.py
+++ base64.py
@@ -13,13 +13,21 @@ def base64_map():
   #print(map_char_to_int)
 
 #input
-st = "abc"
-bin_input = ''.join('{0:08b}'.format(ord(x), 'b') for x in st)
-print(bin_input)
-bin_list = [bin_input[i:i+6] for i in range(0,len(bin_input), 6)]
-int_list = [int(x, 2) for x in bin_list]
-print(int_list)
-mapping = base64_map()
-retorno = [mapping[1][x] for x in int_list]
-retorno = ''.join(retorno)
-print(retorno)
+def base_64_encoder(str:string, e=True):
+  #str = "abc"
+  size = 6
+  select = 1
+  if not e:
+    size = 8
+    select = 0
+  bin_input = ''.join('{0:08b}'.format(ord(x), 'b') for x in str)
+  print(bin_input)
+  bin_list = [bin_input[i:i+size] for i in range(0,len(bin_input), size)]
+  int_list = [int(x, 2) for x in bin_list]
+  print(int_list)
+  mapping = base64_map()
+  retorno = [mapping[select][x] for x in int_list]
+  retorno = ''.join(retorno)
+  print(retorno)
+
+base_64_encoder("abc")
