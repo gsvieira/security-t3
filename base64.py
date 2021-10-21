71b2aecf838ddfa8eac5fbbdcd2af417fb3697ff
gabriel_s_vieira <gsvieira98@gmail.com>
2021-10-21 11:12:41 -0300

    base64_decoder fix

--- base64.py
+++ base64.py
@@ -13,21 +13,29 @@ def base64_map():
   #print(map_char_to_int)
 
 #input
-def base_64_encoder(str:string, e=True):
+def base64_encoder(str:string):
   #str = "abc"
-  size = 6
-  select = 1
-  if not e:
-    size = 8
-    select = 0
   bin_input = ''.join('{0:08b}'.format(ord(x), 'b') for x in str)
   print(bin_input)
-  bin_list = [bin_input[i:i+size] for i in range(0,len(bin_input), size)]
+  bin_list = [bin_input[i:i+6] for i in range(0,len(bin_input), 6)]
   int_list = [int(x, 2) for x in bin_list]
   print(int_list)
   mapping = base64_map()
-  retorno = [mapping[select][x] for x in int_list]
+  retorno = [mapping[1][x] for x in int_list]
   retorno = ''.join(retorno)
   print(retorno)
 
-base_64_encoder("abc")
+
+def base64_decoder(str:string):
+  mapping = base64_map()
+  int_list = ['{0:06b}'.format(mapping[0][x]) for x in str]
+  #print(int_list)
+  str = ''.join(int_list)
+  bin_list = [str[i:i+8] for i in range(0,len(str), 8)]
+  #print(bin_list)
+  int_list = [chr(int(x, 2)) for x in bin_list]
+  print(int_list)
+  return ''.join(int_list)
+
+base64_encoder("abc")
+print(base64_decoder("YWJj"))
